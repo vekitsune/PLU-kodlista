bcd21d5d4f5d973f38292799ced23b00481b031a
vekitsune <48123861+vekitsune@users.noreply.github.com>
2019-09-11 17:41:09 +0200

    project mukodokepes

---
+++ src/plukodgyakorlo2/TermekKodParos.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plukodgyakorlo2;
+
+import java.io.*;
+
+/**
+ *
+ * @author Verosffy
+ */
+public class TermekKodParos implements Serializable{
+    private String termeknev;
+    private String termekkod;
+
+    public TermekKodParos(String termeknev, String termekkod) {
+        this.termeknev = termeknev;
+        this.termekkod = termekkod;
+    }
+    public boolean egyformae(TermekKodParos termekKOdParos){
+        if(this.termeknev.equalsIgnoreCase(termekKOdParos.termeknev)&&
+                this.termekkod==termekKOdParos.termekkod){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String getTermekkod() {
+        return termekkod;
+    }
+
+    public void setTermekkod(String termekkod) {
+        this.termekkod = termekkod;
+    }
+
+    public String getTermeknev() {
+        return termeknev;
+    }
+
+    public void setTermeknev(String termeknev) {
+        this.termeknev = termeknev;
+    }
+    
+    
+}
